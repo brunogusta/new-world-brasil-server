5d0c1ba87d3e0c230b97b387b82440a33dc3530c
Bruno Gustavo <brugucolosil95@gmail.com>
2020-04-28 14:57:18 -0300

    refactor: change user schema and token schema

---
+++ src/models/tokenSchema.js
@@ -0,0 +1,23 @@
+import mongoose from '../database';
+
+const tokenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    expires: 43200,
+  },
+});
+
+const token = mongoose.model('Token', tokenSchema);
+
+export default token;
---
+++ src/models/userSchema.js
@@ -0,0 +1,56 @@
+const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isSupporter: {
+    type: Boolean,
+    default: false,
+  },
+  passwordResetToken: {
+    type: String,
+    select: false,
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre('save', async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
