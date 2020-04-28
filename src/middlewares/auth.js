9f578b0e6f976b85ede6e3ac4e5cfe3289cd3bac
Bruno Gustavo <brugucolosil95@gmail.com>
2020-04-28 14:57:48 -0300

    feat: add auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res
+      .status(401)
+      .send({ error: 'Um erro inesperado ocorreu, faça login novamente' });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (!parts.length === 2) {
+    return res
+      .status(401)
+      .send({ error: 'Um erro inesperado ocorreu, faça login novamente' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res
+      .status(401)
+      .send({ error: 'Um erro inesperado ocorreu, faça login novamente' });
+  }
+
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err)
+      return res
+        .status(401)
+        .send({ error: 'Um erro inesperado ocorreu, faça login novamente' });
+
+    req.userId = decoded.id;
+    req.email = decoded.email;
+    req.isSupporter = decoded.supporter;
+    return next();
+  });
+};
